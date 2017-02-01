c69ed146c1ad99b4a9e0952f69f7f704e3ddd23c
paul604 <paulorhon@live.fr>
2017-02-01 15:31:35 +0100

    exo1 q4

--- TD1/exo1/js.js
+++ TD1/exo1/js.js
@@ -2,9 +2,13 @@ function loadQ1(){
   window.alert("coucou");
 }
 
+//------------------------------------
+
 function loadQ2(){
   window.alert(navigator.appName+navigator.appVersion);
 }
+
+//------------------------------------
 
 function loadQ3(){
   var bOn =document.getElementsByName("buttonOn")[0];
@@ -26,4 +30,15 @@ function OnOff(bool, img){
     img.src="lpoff.jpg";
   }
   return bool;
+}
+
+//------------------------------------
+
+function loadQ4(){
+
+  document.getElementsByName("button")[0].addEventListener("click",function(){
+    var text = document.getElementsByName("value")[0];
+    window.alert(text.value);
+    text.value ="";
+  });
 }
