40d4d95f39e122e162d1b70b0080e2d52727abf8
paul604 <paulorhon@live.fr>
2017-02-01 14:47:42 +0100

    exo 1 q3

--- TD1/exo1/js.js
+++ TD1/exo1/js.js
@@ -5,3 +5,19 @@ function loadQ1(){
 function loadQ2(){
   window.alert(navigator.appName+navigator.appVersion);
 }
+
+function loadQ3(){
+  document.getElementsByName("buttonOn")[0].addEventListener("click",function(){OnOff(true);});
+  document.getElementsByName("buttonOff")[0].addEventListener("click",function(){OnOff(false);});
+}
+
+function OnOff(bool){
+  var img = document.getElementById("img");
+  if(bool){
+    img.src="lpon.jpg";
+    return true;
+  }else{
+    img.src="lpoff.jpg";
+    return false;
+  }
+}
