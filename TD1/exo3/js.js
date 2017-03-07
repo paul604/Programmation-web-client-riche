dcf8b0076d5e09ca7bd19784095c27f18ec65152
paul604 <paulorhon@live.fr>
2017-03-07 14:50:41 +0100

    exo 3 q3

--- TD1/exo3/js.js
+++ TD1/exo3/js.js
@@ -1,6 +1,6 @@
 function loadQ1() {
   console.log("log");
-  document.getElementById("form").addEventListener("submit", function (event) {
+  document.getElementById("form").addEventListener("submit", function(event) {
     if(document.getElementsByName('nom')[0].value=="" || document.getElementsByName('Prenom')[0].value==""){
       alert("error");
       event.preventDefault();
@@ -12,28 +12,42 @@ function loadQ2() {
   console.log("log");
   for (var val of document.getElementsByClassName("input")) {
     console.log("log");
-    val.addEventListener("blur", function () {
-      if(this.value==""){
-        this.style.border= " bottom 2px solid red";
-      }else{
-        this.style.border= "none";
-      }
-    });
+    val.addEventListener("blur", function(){estVid(this)});
   }
+
   document.getElementById("form").addEventListener("submit", function (event) {
-    if(document.getElementsByName('nom')[0].value=="" || document.getElementsByName('Prenom')[0].value==""){
-      // document.getElementById("error").innerHTML="nop"
+    var nom = document.getElementsByName('nom')[0];
+    var prenom = document.getElementsByName('Prenom')[0];
+    if(nom.value=="" || prenom.value==""){
+      testVid(nom);
+      testVid(prenom);
       event.preventDefault();
     }
   });
 }
+function estVid(val){
+  if(val.value==""){
+    val.style.border= "2px solid red";
+    val.style.borderRadius="5px";
+  }else{
+    // val.style.border= "1px solid";
+    val.removeAttribute("style");
+  }
+}
 
 function loadQ3() {
-  console.log("log");
-  document.getElementById("form").addEventListener("submit", function (event) {
-    if(document.getElementsByName('nom')[0].value=="" || document.getElementsByName('Prenom')[0].value==""){
-      alert("error");
-      event.preventDefault();
-    }
-  });
+  for (var val of document.getElementsByClassName("input")) {
+    console.log("log");
+    val.addEventListener("blur", function(){estVid2(this)});
+  }
+}
+function estVid2(val){
+  if(val.value==""){
+    val.style.border= "2px solid red";
+    val.style.borderRadius="5px";
+    document.getElementById(val.id+"E").innerHTML= "error";
+  }else{
+    // val.style.border= "1px solid";
+    val.removeAttribute("style");
+  }
 }
