7d68c6d00e5de59356472608e9837122f189c26a
paul604 <paulorhon@live.fr>
2017-03-08 12:17:11 +0100

    suite exo 3 q3 expressions régulières

--- TD1/exo3/js.js
+++ TD1/exo3/js.js
@@ -8,6 +8,8 @@ function loadQ1() {
   });
 }
 
+//--------------------------------
+
 function loadQ2() {
   console.log("log");
   for (var val of document.getElementsByClassName("input")) {
@@ -35,19 +37,72 @@ function estVid(val){
   }
 }
 
+//--------------------------------
+
 function loadQ3() {
-  for (var val of document.getElementsByTagName("input")) {
-    console.log("log");
-    val.addEventListener("blur", function(){estVid2(this)});
+  var form = document.getElementById("form");
+
+  for (var val of document.getElementsByTagName("input") ) {
+    if(val.type!="radio"){
+      val.addEventListener("blur", function(){estok(this)});
+    }
+  }
+
+  for (var val of document.getElementsByTagName("select")) {
+    val.addEventListener("change", function(){estok(this)});
+  }
+
+  form.addEventListener("submit", function (event) {
+    for (var val of document.getElementsByTagName("input")) {
+      if(val.value=="" || (val.type=="radio" && val.value=="non")){
+        estok(val);
+        event.preventDefault();
+      }
+    }
+  });
+
+}
+function estok(val){
+  if(val.value==""){
+    error(val);
+  }else if (val.classList.contains("string")) {
+    if(/^[a-zA-Z]+(-[a-zA-Z]+)*$/.test(val.value)){
+      ok(val);
+    }else{
+      error(val);
+    }
+  }else if (val.classList.contains("date")) {
+    if(/^(01|[1-3]0|[0-3][1-9])\/0[1-9]|1[0-2]\/[0-9][0-9]$/.test(val.value)){
+      ok(val);
+    }else{
+      error(val);
+    }
+  }else if (val.classList.contains("cp") && document.getElementById('pays').value=="France") {
+    if(/^[0-9]{4}$/.test(val.value)){
+      ok(val);
+    }else{
+      error(val);
+    }
+  }else if (val.classList.contains("pays")) {
+    estok(document.getElementById('cp'));
+  }else if (val.classList.contains("email")) {
+    if(/^[a-zA-z0-9][a-zA-Z0-9.-_]*[a-zA-Z0-9]*@[a-zA-Z0-9][a-zA-Z0-9.-_]*[a-zA-Z0-9]*\.[a-z]+$/.test(val.value)){
+      ok(val);
+    }else{
+      error(val);
+    }
+  }else{
+    ok(val);
   }
 }
-function estVid2(val){
-  if(val.value==""){
-    val.style.border= "2px solid red";
-    val.style.borderRadius="5px";
-    document.getElementById(val.id+"E").innerHTML= "error";
-  }else{
-    // val.style.border= "1px solid";
-    val.removeAttribute("style");
-  }
+function error(obj){
+  console.log("error");
+  obj.style.border= "2px solid red";
+  obj.style.borderRadius="5px";
+  document.getElementById(obj.id+"E").innerHTML= "error";
+}
+function ok(obj){
+  console.log("ok");
+  obj.removeAttribute("style");
+  document.getElementById(obj.id+"E").innerHTML= "";
 }
