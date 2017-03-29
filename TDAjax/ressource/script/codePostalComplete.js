28ca7b6bb61e2309ff2c1d16349be0c54fef0839
paul604 <paulorhon@live.fr>
2017-03-29 13:56:25 +0200

    ajax complete

--- TDAjax/ressource/script/codePostalComplete.js
+++ TDAjax/ressource/script/codePostalComplete.js
@@ -1,13 +1,17 @@
 $(function(argument) {
-  var list = ["azertyuiop", "zyuiop", "ancenis", "nantes"];
-  $("#commune").autocomplete({
-    minLength: 3,
-    source: list,
-    select: function( event, ui ) {
-      console.log(event);
-      console.log(ui);
-      console.log(ui.item.value);
-      $("div").html(ui.item.value);
-    }
-  });
+  // a)
+  // var list = ["Bordeaux", "zyuiop", "ancenis", "nantes"];
+  // $("#commune").autocomplete({
+  //   minLength: 3,
+  //   source: list,
+  //   select: function( event, ui ) {
+  //     console.log(event);
+  //     console.log(ui);
+  //     console.log(ui.item.value);
+  //     $("div").html(ui.item.value);
+  //   }
+  // });
+
+  //b)
+  
 });
